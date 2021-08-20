3393857811520f6b6af5fd1b086bacc494730a85
dshetty1 <60328981+dshetty1@users.noreply.github.com>
2021-08-20 17:31:46 -0500

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,9 +1,7 @@
 #ifndef BST_H_
 #define BST_H_
 
-/* A node structure for use with the
- * binary search tree.
- */
+// A node structure for use with the binary search tree
 typedef struct  bst_node {
     char * data;
     struct bst_node * right;
@@ -11,54 +9,21 @@ typedef struct  bst_node {
 } bst_node ;
 
 
-/* Adds a value to the binary search tree.
- * Parameters:
- *  root - the pointer to the root of the tree
- * Return value:
- *  none
- */
-void add ( bst_node ** root, char * word );
+// Adds a value to the binary search tree
+void add (bst_node ** root, char * word);
 
 
-/* Prints the values stored in the bst to standard output according to
- * the inorder traversal. The values should be separated
- * by a single space.
- * Parameters:
- *  root - the root of the tree
- * Return value:
- *  none
- */
-void inorder ( bst_node * root ) ;
+// Prints the values stored in the bst the output according to inorder traversal
+void inorder (bst_node * root);
 
-/* Removes the node with the smallest value
- * in the tree.
- * Parameters:
- *  root - the pointer to the root of the tree
- * Return value:
- *  the string from the removed node or
- *  NULL if no node was removed.
- */
-char * removeSmallest (  bst_node ** root );
+// Removes the node with the smallest value in the tree
+char * removeSmallest (bst_node ** root);
 
-/* Removes the node with the largest value
- * in the tree.
- * Parameters:
- *  root - the pointer to the root of the tree
- * Return value:
- *  the string from the removed node or
- *  NULL if no node was removed.
- */
-char * removeLargest (  bst_node ** root );
+// Removes the node with the largest value in the tree
+char * removeLargest (bst_node ** root);
 
 
-
-///////////////////////////////////
-// Add other declarations below. //
-///////////////////////////////////
-
-/*
- *Declaration for the new_node constructor
-*/
+// Declaration for the new_node constructor
 struct bst_node* new_node(char * input);
 
 
